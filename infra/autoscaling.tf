2887303c1aa651f64f554497ce5876b261462a49
fernandes <contatomatheusfernandeeess@gmail.com>
2026-03-12 18:45:00 -0300

    feat: Add autoscaling configuration for ECS infrastructure.

--- infra/autoscaling.tf
+++ infra/autoscaling.tf
@@ -8,8 +8,8 @@ resource "aws_appautoscaling_target" "ms_video" {
   service_namespace  = "ecs"
   resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.ms_video.name}"
   scalable_dimension = "ecs:service:DesiredCount"
-  min_capacity       = 1
-  max_capacity       = 2
+  min_capacity       = 2
+  max_capacity       = 4
 }
 
 resource "aws_appautoscaling_policy" "ms_video_cpu" {
